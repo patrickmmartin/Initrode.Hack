92182d3396ca8e42bd33323cb4a1398a001e5032
Patrick Martin <patrickmmartin@gmail.com>
2015-06-13 17:32:22 +0100

    adding rudimentary build system

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+
+clean:
+	msbuild "Initrode.Hack().groupproj" /target:clean
+
+build:
+	msbuild "Initrode.Hack().groupproj" /target:build
+
---
+++ rsvars.bat
@@ -0,0 +1,8 @@
+@SET BDS=C:\Program Files (x86)\CodeGear\RAD Studio\6.0
+@SET BDSCOMMONDIR=C:\Users\Public\Documents\RAD Studio\6.0
+@SET FrameworkDir=C:\Windows\Microsoft.NET\Framework\v2.0.50727
+@SET FrameworkVersion=v2.0.50727
+@SET FrameworkSDKDir=
+@SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
+
+
